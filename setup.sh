2bbc31443ec4fec88172724ecbb800a66afd5883
McAfee <pjm4439@rit.edu>
2014-08-22 18:00:03 -0400

    Actually removed copied folders

--- setup.sh
+++
@@ -1,8 +0,0 @@
-wget http://ssebuild.cased.de/nightly/soot/lib/soot-trunk.jar
-wget http://ssebuild.cased.de/nightly/soot-infoflow/build/jar/soot-infoflow.jar
-wget http://ssebuild.cased.de/nightly/soot-infoflow-android/build/jar/soot-infoflow-android.jar
-wget https://github.com/secure-software-engineering/soot-infoflow-android/raw/develop/lib/slf4j-simple-1.7.5.jar
-wget https://github.com/secure-software-engineering/soot-infoflow-android/raw/develop/lib/axml-2.0.jar
-wget http://ssebuild.cased.de/nightly/soot-infoflow-android/SourcesAndSinks.txt
-wget http://ssebuild.cased.de/nightly/soot-infoflow/EasyTaintWrapperSource.txt
-wget http://ssebuild.cased.de/nightly/soot-infoflow-android/AndroidCallbacks.txt
