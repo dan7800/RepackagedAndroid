990999712951df6b7cfb45e2354ba7b321aea17d
Patrick McAfee <McAfee@Agent-Smith.local>
2014-08-21 21:54:36 -0400

    fixed script to be cleaner (also to work)

--- run.sh
+++ run.sh
@@ -1,1 +1,13 @@
-java -Xms1024M  -Xmx3G -cp "soot-trunk.jar:soot-infoflow.jar:soot-infoflow-android.jar:slf4j-api-1.7.5.jar:slf4j-simple-1.7.5.jar:axml-2.0.jar" soot.jimple.infoflow.android.TestApps.Test "$1" "/home/mcafee/IDEs/adt-bundle-linux-x86_64-20140702/sdk/platforms" > $2
+#!/bin/bash
+
+#Usage: run.sh AppToTarget AndroidSDKPlatformsDirectory FileOutputName
+
+#breakdown
+#JAVA_ARGS= --Xmx3G
+CLASSPATH="soot-trunk.jar:soot-infoflow.jar:soot-infoflow-android.jar:slf4j-api-1.7.5.jar:slf4j-simple-1.7.5.jar:axml-2.0.jar"
+FLOWDROID="soot.jimple.infoflow.android.TestApps.Test"
+TARGET_APP="$1"
+ANDROID_SDK_PLATFORMS="$2"
+OUTPUT_FILE="$3"
+#finally, the command itself
+java -cp $CLASSPATH $FLOWDROID $TARGET_APP $ANDROID_SDK_PLATFORMS > $OUTPUT_FILE
