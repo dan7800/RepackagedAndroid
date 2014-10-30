9128564e27259e3c2b82b746f2b343500bd7effd
Patrick McAfee <McAfee@Agent-Smith.local>
2014-10-29 22:08:17 -0400

    slightly more robustness

--- AnalysisEngine/AndroidAnalysisEngine/src/main/java/com/rit/AndroidAnalysisEngine/engine/SourceWriter.java
+++ AnalysisEngine/AndroidAnalysisEngine/src/main/java/com/rit/AndroidAnalysisEngine/engine/SourceWriter.java
@@ -30,7 +30,7 @@ public class SourceWriter {
 	public String getFunctionCalls(){
 		String functionLines = "";
 		Iterator<Class<? extends Activity>> rator= activityClasses.iterator();
-		//functionLines += "\t\tBundle bundle = new Bundle();\n";
+		functionLines += "\t\tBundle bundle = new Bundle();\n";
 		Class<? extends Activity> curAct;
         while (rator.hasNext()){
         	curAct = rator.next();
@@ -39,7 +39,7 @@ public class SourceWriter {
         	}
         	functionLines += "\t\t" + curAct.getSimpleName() + " "+curAct.getSimpleName()+"Var = "+
         			"new "+curAct.getSimpleName()+"();\n";
-        	functionLines += "\t\t" + curAct.getSimpleName()+"Var"+".onCreate(null);\n";
+        	functionLines += "\t\t" + curAct.getSimpleName()+"Var"+".onCreate(bundle);\n";
         }
 		
 		return functionLines;
