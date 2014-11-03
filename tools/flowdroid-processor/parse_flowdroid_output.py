0245d39dea741fd3e8686f8f5d107c58c06f922e
Justin Peterson <jmp3833@rit.edu>
2014-11-03 16:18:04 -0500

    Add tool output to supplement test results

--- tools/flowdroid-processor/parse_flowdroid_output.py
+++ tools/flowdroid-processor/parse_flowdroid_output.py
@@ -2,9 +2,19 @@
 #Parse the output files for the FlowDroid platform for 
 #for any source to sink connections. 
 import sys
+import os
 
-with open(sys.argv[1]) as f:
-   #print flow to sink connections in analysis
-   for line in f:
-   		if 'Found a flow to sink' in line or '- ' in line:
-   			print line
+for textFile in os.listdir(sys.argv[1]):
+	textFilePath = os.path.join(sys.argv[1], textFile)
+	with open(textFilePath) as f:
+		outputString = ''
+		#print flow to sink connections in analysis
+		for line in f:
+			if 'Found a flow to sink' in line or '- ' in line:
+				outputString = outputString + line
+
+		file = open(textFilePath, 'a')
+		file.write("\n\n\nCondensed report: \n" + outputString)
+		file.close()
+
+print("Completed output parsing and appended to bottom of original text files")
--- tools/flowdroid-processor/run.py
+++ tools/flowdroid-processor/run.py
@@ -7,11 +7,18 @@ import sys
 jarfiles = 'soot-trunk.jar:soot-infoflow.jar:soot-infoflow-android.jar:slf4j-api-1.7.5.jar:slf4j-simple-1.7.5.jar:axml-2.0.jar soot.jimple.infoflow.android.TestApps.Test'
 platformsDir = "/Users/justinpeterson/Android/android-sdk-macosx/platforms"
 
-apkFile = sys.argv[1]
-apkFilename = os.path.basename(apkFile)
-apkFiletxt = 'output/' + os.path.splitext(apkFilename)[0] + '.txt'
+def runOnDirectory(dir):
+	apkFileDir = dir
+	for apkFile in os.listdir(apkFileDir):
+		
+		apkFile = os.path.join(dir, apkFile)
 
-file = open(apkFiletxt, 'w')
-file.close()	
+		apkFilename = os.path.basename(apkFile)
+		apkFiletxt = 'output/' + os.path.splitext(apkFilename)[0] + '.txt'
 
-os.system('java -cp ' + jarfiles + ' "' + apkFile + '" ' + platformsDir + '> ' + apkFiletxt)
+		file = open(apkFiletxt, 'w')
+		file.close()	
+
+		os.system('java -cp ' + jarfiles + ' "' + apkFile + '" ' + platformsDir + '> ' + apkFiletxt)
+
+runOnDirectory(sys.argv[1])
